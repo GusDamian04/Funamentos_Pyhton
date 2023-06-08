dc3f456f2483dd3edb63b345eca20fba929da343
Gustavo Angel Damian Gonzalez <damiangustavoangel@gmail.com>
2023-06-08 14:32:57 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,14 @@
+#Python es No tipado
+name = "Gustavo"
+age = 18
+is_single = False
+print(name)
+print(age)
+print(is_single)
+"""
+Hola soy Gustavo tengo 18 años y soltero = True
+"""
+#utilizando forma de String
+print("Hola soy {}, tengo {} años y soltero = {}".
+format(name, age, is_single))
+print(f"Hola soy {name}, tengo {age} años y soltero = {is_single}")
