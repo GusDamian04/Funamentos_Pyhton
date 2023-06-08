9f86261b8bf3cba445ea79ebac4d963364533e51
Gustavo Angel Damian Gonzalez <damiangustavoangel@gmail.com>
2023-06-08 14:43:29 -0600

    03 Operadores

---
+++ operadores.py
@@ -0,0 +1,18 @@
+# Operadores Matematicos
+# + - * %
+# / División flotante
+# // División entera
+# ** Potencia
+print(24 + 62)
+print(50 - 32)
+print(35 * 2)
+print(10 % 3)
+print(10 / 3)
+print(10 // 3)
+print(3 ** 3)
+
+# Operadores String
+# + para contatenar
+# * replicar un String
+print("Hola a " + "todos!")
+print("DSM 3A" * 4)
