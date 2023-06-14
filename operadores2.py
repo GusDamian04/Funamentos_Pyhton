df055df958424ee24f3f4d7a7f059ec870f4008c
Gustavo Angel Damian Gonzalez <damiangustavoangel@gmail.com>
2023-06-14 12:35:48 -0600

    05 Operadores Logicos y Boleanos

---
+++ operadores2.py
@@ -0,0 +1,23 @@
+#operadores booleanos
+print("Operadores Booleanos")
+# < <=
+print(10<10)
+print(10<=10)
+# > >=
+print(10>10)
+print(10>=10)
+# ==
+print(10==10)
+#!=
+print(10!=10)
+#Operadores Logicos
+print("Operadores Logicos")
+# and (True ifall true)
+print(True and True)
+print(True and False)
+# or (True if at least one True)
+print(False or False)
+print(False or True)
+# Not True
+print(not True)
+print(not False)
