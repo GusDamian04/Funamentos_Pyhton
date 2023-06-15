5f2fb7560ef9541eea617703268d8fc7583d6d72
Gustavo Angel Damian Gonzalez <damiangustavoangel@gmail.com>
2023-06-15 13:39:46 -0600

    10 Diccionarios

---
+++ diccionarios.py
@@ -0,0 +1,17 @@
+# Diccionario
+# { "Key": "value", "key": "value" }
+
+team = {
+    "name": "City",
+    "country": "England",
+    "champions": 1,
+    "players": ['halland', 'grealinsh']
+}
+print(type(team))
+print(team)
+print(team["name"])
+print(team["players"])
+team["players"].append("Kevin")
+team["name"] = "Manchester City"
+team["leage"] = "Premiere league"
+print(team)
